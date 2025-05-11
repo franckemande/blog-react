77c5f62e270fc73449287346d2550ef4f33c58d3
franck alex <franckemande@gmail.com>
2025-05-11 17:26:05 +0100

    initialisation du projet

---
+++ src/header/Header.jsx
@@ -0,0 +1,17 @@
+import './header.css'
+
+export default function Header() {
+  return (
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headerTitlesm">react & node</span>
+            <span className="headerTitlelg">blog</span>
+        </div>
+        <img 
+          src="https://www.autoworldjournal.com/wp-content/uploads/2024/04/A-Look-at-the-World-Automobile-Industry-in-2030-3.jpg" 
+          alt="" 
+          className="headerimg" 
+          />
+    </div>
+  )
+}
