06901cb6e16116330de43aaa1192019e87394135
franck alex <franckemande@gmail.com>
2025-05-11 01:19:45 +0100

    commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../../header/Header";
+import "./home.css";
+import Sidebar from "../../sidebar/Sidebar";
+import Posts from "../../posts/Posts";
+
+export default function Home() {
+  return (
+  <>
+    <Header/>
+   <div className="home">
+     <Posts />
+     <Sidebar/>
+   </div>
+   
+  </>
+  );
+}
